44214c0425bdc063357e954959782b76156c3d3c
kmsclass <47582514+kmsclass@users.noreply.github.com>
2026-09-16 17:02:08 +0900

    20260916 테스트 문제

---
+++ test0916.py
@@ -0,0 +1,23 @@
+"""
+1.sales_2015.xlsx 파일의  january_2015 sheet의 중
+ 열이  "Customer Name", "Sale Amount" 컬럼만 
+ sales_2015_amt.xlsx 파일로 저장하기  
+"""
+i
+
+"""
+2. supplier_data.csv 파일을 
+   pandas를 이용하여 읽고 Purchase Date 컬럼의 값이 1/20/14인 데이터만 
+   140120_data.csv 파일로 저장하기
+"""
+'''
+ 3. 시도별 전출입 인구수.xlsx 파일을 읽어 다음 그래프를 작성하기
+    전출지가 서울특별시이고 전입지가 "전국","경기도","부산광역시" 데이터를 
+    읽어, 20210913-1.png 그래프 작성하기
+''' 
+
+'''
+  4. sales_2015.xlsx 파일의 january_2015 sheet 을 읽어 
+    Customer Name별 Sale Amount 를 선그래프로 시각화 하기
+    20210913-2.png 그래프 작성하기
+'''
