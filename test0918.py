2902b0a3fba066706523a439f977d9a8521492aa
kmsclass <47582514+kmsclass@users.noreply.github.com>
2026-09-18 17:09:38 +0900

    20260918 문제

---
+++ test0918.py
@@ -0,0 +1,33 @@
+
+'''
+1. seaborn 모듈의 titanic 데이터를 이용하여 클래스별 생존 인원을 출력하시오
+'''
+
+'''
+2. 멕시코풍 프랜차이즈 Chipotle의 주문 데이터를 이용하여 문제 풀기
+    Chicken Bowl을 2개 이상 주문한 주문 횟수 구하기
+    주문번호    Chicken Bowl 주문수량
+       1                 2
+       2                 3
+       3                 1
+         주문횟수 :   2    1,2,번주문만 횟수 
+'''
+
+'''
+3. drinks.csv 데이터를 읽고,대륙별 술 소비량 대비 알콜 비율 컬럼(alcohol_rate_continent) 구하기
+    alcohol_rate_continent = 대륙별 alcohol/대륙별전체음료소비량
+   으로 계산한다.
+[결과]
+  country continent alcohol
+0 Afghanistan AS    0.020293
+1 Bahrain       AS    0.020293
+2 Bangladesh  AS    0.020293
+3 Bhutan        AS    0.020293
+4 Brunei         AS    0.020293
+'''
+
+'''
+ 4. 전체 평균보다 적은 알코올을 섭취하는 대륙 중에서, spirit을 가장 많이 마시는 국가 구하기
+[결과]
+Russian Federation
+'''
