6684610e3360699f417a8789e542620bf3b7e285
kmsclass <47582514+kmsclass@users.noreply.github.com>
2026-09-22 17:14:23 +0900

    20260922 테스트 문제

---
+++  test0922.py
@@ -0,0 +1,23 @@
+'''
+https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv
+
+저장된 winequality-white.csv 파일을 읽어
+whitewine의 등급(quality)을 분류하는 모델을 구현하기
+
+혼동행렬,정확도,정밀도, 재현율를 출력하기
+
+winequality-white.csv의 컬럼 정보
+
+1 - fixed acidity : 주석산농도
+2 - volatile acidity : 아세트산농도
+3 - citric acid : 구연산농도
+4 - residual sugar : 잔류당분농도
+5 - chlorides : 염화나트륨농도
+6 - free sulfur dioxide : 유리 아황산 농도
+7 - total sulfur dioxide : 총 아황산 농도
+8 - density : 밀도
+9 - pH : ph
+10 - sulphates : 황산칼륨 농도
+11 - alcohol : 알코올 도수
+12 - quality (score between 0 and 10) : 와인등급
+'''
